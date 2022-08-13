6184d18d5de088275db3c453bf4116f64d457f53
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-08-13 13:36:27 +0200

    Update Solution Reference.cpp

--- Solution Reference.cpp
+++ Solution Reference.cpp
@@ -5,22 +5,22 @@ using namespace std;
 class RLEIterator {
     
     inline static const int NOT_FOUND = -1;
-    vector<int>& referenceCurrent;
+    vector<int>& referenceEncoding;
     int index{};
 
 public:
-    RLEIterator(vector<int>& encoding) : referenceCurrent {encoding}{}
+    RLEIterator(vector<int>& encoding) : referenceEncoding {encoding}{}
 
     int next(int nextNthElement) {
-        while (index < referenceCurrent.size() - 1 && referenceCurrent[index] - nextNthElement < 0) {
-            nextNthElement -= referenceCurrent[index];
+        while (index < referenceEncoding.size() - 1 && referenceEncoding[index] - nextNthElement < 0) {
+            nextNthElement -= referenceEncoding[index];
             index += 2;
         }
-        if (index == referenceCurrent.size()) {
+        if (index == referenceEncoding.size()) {
             return NOT_FOUND;
         }
 
-        referenceCurrent[index] -= nextNthElement;
-        return referenceCurrent[index + 1];
+        referenceEncoding[index] -= nextNthElement;
+        return referenceEncoding[index + 1];
     }
 };
