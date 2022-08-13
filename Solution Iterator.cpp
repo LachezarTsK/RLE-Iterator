8aee8c4c691e40c8ac4a74c1c1a362d96010b6a5
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-08-13 13:35:22 +0200

    Update Solution Iterator.cpp

--- Solution Iterator.cpp
+++ Solution Iterator.cpp
@@ -6,25 +6,25 @@ using namespace std;
 class RLEIterator {
     
     inline static const int NOT_FOUND = -1;
-    vector<int>::iterator iteratorCurrent;
+    vector<int>::iterator iteratorEncoding;
     vector<int>::iterator end;
 
 public:
     RLEIterator(vector<int>& encoding) {
-        this->iteratorCurrent = encoding.begin();
+        this->iteratorEncoding = encoding.begin();
         this->end = encoding.end();
     }
 
     int next(int nextNthElement) {
-        while (iteratorCurrent < end - 1 && *iteratorCurrent - nextNthElement < 0) {
-            nextNthElement -= *iteratorCurrent;
-            iteratorCurrent += 2;
+        while (iteratorEncoding < end - 1 && *iteratorEncoding - nextNthElement < 0) {
+            nextNthElement -= *iteratorEncoding;
+            iteratorEncoding += 2;
         }
-        if (iteratorCurrent == end) {
+        if (iteratorEncoding == end) {
             return NOT_FOUND;
         }
 
-        *iteratorCurrent -= nextNthElement;
-        return *(iteratorCurrent + 1);
+        *iteratorEncoding -= nextNthElement;
+        return *(iteratorEncoding + 1);
     }
 };
